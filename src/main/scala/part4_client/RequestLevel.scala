31cf65e823c6e3e07ce35f51c6d41f164f83b807
marwhals <marjan.mubarok2012@gmail.com>
2026-02-09 08:39:44 +0000

    The request level client API

---
+++ src/main/scala/part4_client/RequestLevel.scala
@@ -0,0 +1,69 @@
+package part4_client
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity, HttpMethods, HttpRequest}
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.Source
+import spray.json._
+
+import scala.util.{Failure, Success}
+
+/**
+ * Notes request level API
+ * - Simple to use
+ * - No need to manage anything like connections pools etc
+ *
+ * --------
+ * Best for:
+ * - low volume, low-latency requests
+ *
+ * Do not use the request-level API for:
+ * - high volume (use the host-level API)
+ * - long-lived requests (use the connection-level API)
+ *
+ */
+
+object RequestLevel extends App with PaymentJsonProtocol {
+
+  implicit val system: ActorSystem = ActorSystem("HostLevel")
+  implicit val materializer = ActorMaterializer()
+  import system.dispatcher
+
+  val responseFuture = Http().singleRequest(HttpRequest(uri = "http://www.google.com"))
+
+  responseFuture.onComplete {
+    case Success(response) =>
+      // VERY IMPORTANT
+      response.discardEntityBytes()
+      println(s"The request was successful and returned: $response")
+    case Failure(ex) =>
+      println(s"The request failed with: $ex")
+  }
+
+  import PaymentSystemDomain._
+
+  val creditCards = List(
+    CreditCard("4242-4242-4242-4242", "424", "tx-test-account"),
+    CreditCard("1234-1234-1234-1234", "123", "tx-daniels-account"),
+    CreditCard("1234-1234-4321-4321", "321", "my-awesome-account")
+  )
+
+  val paymentRequests = creditCards.map(creditCard => PaymentRequest(creditCard, "rtjvm-store-account", 99))
+  val serverHttpRequests = paymentRequests.map(paymentRequest =>
+    HttpRequest(
+      HttpMethods.POST,
+      uri = "http://localhost:8080/api/payments",
+      entity = HttpEntity(
+        ContentTypes.`application/json`,
+        paymentRequest.toJson.prettyPrint
+      )
+    )
+  )
+
+  Source(serverHttpRequests)
+    .mapAsyncUnordered(10)(request => Http().singleRequest(request))
+    .runForeach(println)
+
+
+}
